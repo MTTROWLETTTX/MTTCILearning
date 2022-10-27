8e29d280c85c85d2bbe0af8f0d744b0dbdc46f8f
MTTROWLETTTX <110212563+MTTROWLETTTX@users.noreply.github.com>
2022-10-27 16:39:20 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,6 @@ namespace MTTCILearning
 {
     public class Program
     {
-        a;
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
